f002030159ce0927db482cbea77f100319f785fa
Jesus Zuñiga <jesuszt@hotmail.com>
2023-06-14 16:38:25 -0600

    Se agrega icono para la pantalla de Login

--- src/UsuariosApp.jsx
+++ src/UsuariosApp.jsx
@@ -5,13 +5,13 @@ export const UsuariosApp = () => {
                 <div id="formContent">
 
                     <div className="fadeIn first">
-                        <img src="/usuarios-alt.png" id="icon" alt="User Icon" />
+                        <img src="/circulo-de-usuario.svg" id="icon" alt="User Icon" />
                     </div>
 
                     <form>
-                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
-                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
-                        <input type="submit" className="fadeIn fourth" value="Log In" />
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="Usuario" />
+                        <input type="text" id="password" className="fadeIn third" name="password" placeholder="Contraseña" />
+                        <input type="submit" className="fadeIn fourth" value="Entrar" />
                     </form>
 
                 </div>
